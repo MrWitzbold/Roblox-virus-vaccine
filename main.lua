0bf46cb972c44e6eb7ea372e21a6e72da88f49af
JUKjacker <80601981+MrWitzbold@users.noreply.github.com>
2021-09-17 21:34:00 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,11 @@
+print("This game is protected by JUK's vaccine")
+print("Learn to protect your game by joining: https://discord.gg/4jFcGAaq2T")
+
+
 -- First a simple quick scan
 
 for i,v in pairs(workspace:GetChildren()) do
 	if v:FindFirstChild("Lag-Players") then
-		print("Removing lag script...")
 		v:FindFirstChild("Lag-Players"):Remove()
 	end
 end
@@ -12,33 +15,24 @@ end
 function iterate_in(object)
 	for i,v in pairs(object:GetChildren()) do
 		if v.Name == "Infected" and v:IsA("Script") then
-			print("Found infector script")
 			v.Disabled = true
 		end
 		
 		if v.Name == "Infected" and v:IsA("Script") == false then
-			print("Changing infected name")
 			v.Name = tostring(v.ClassName)
 		end
 		
 		if v.Name == "Lag-Players" then
-			print("Found a lag script")
 			v:Remove()
 		end
 
 		if v.Name == "Spread" or v.Name == "ProperGr�mmerNeededInPhilosiphalLocations;insertNoobHere" then
-			print("Found a spread script")
 			v.Disabled = true
 		end
 		
 		if v:IsA("Model") or v:IsA("Folder") or v:IsA("Fire") or v:IsA("Part") then
 			wait(0)
 			iterate_in(v)
-			print("Going deeper...")
-		end
-		
-		if v.Name == "Hedge" then
-			print("In hedge")
 		end
 	end
 end
@@ -48,22 +42,18 @@ wait(5)
 iterate_in(workspace)
 
 -- Then you gotta actually get rid of the spreads, The above thing just disables them
-print("NOW DELETING THE SPREADS")
 function get_rid_of_spreads(object)
 	for i,v in pairs(object:GetChildren()) do
 		if v:IsA("Fire") and v:FindFirstChild("Spread") ~= nil then
-			print("Found a fire with spread inside")
 			v:Remove()
 		end
 		
 		if v.Name == "Spread" or v.Name == "ProperGr�mmerNeededInPhilosiphalLocations;insertNoobHere" then
-			print("Found a spread script (deleting it)")
 			v:Remove()
 		end
 		
 		if v:IsA("Model") or v:IsA("Folder") or v:IsA("Fire") or v:IsA("Part") then
 			wait(0)
-			print("Going deeper...")
 			get_rid_of_spreads(v)
 		end
 	end
@@ -73,13 +63,11 @@ get_rid_of_spreads(workspace)
 
 for i,v in pairs(workspace:GetChildren()) do
 	if v.Name == "Infected" then
-		print("Changing infected name")
 		v.Name = tostring(v.ClassName)
 	end
 	
 	for j,w in pairs(v:GetChildren()) do
 		if w.Name == "Infected" then
-			print("Changing infected name")
 			w.Name = tostring(v.ClassName)
 		end
 	end
